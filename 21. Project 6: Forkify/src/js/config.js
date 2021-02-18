09cc5cca4fcabe3a1f573643165498be480743a5
Sam <40251608+sams248@users.noreply.github.com>
2021-02-17 19:06:30 -0500

    Add files via upload

---
+++ 21. Project 6: Forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3362f40b-9444-4f6e-bce2-a998c94adfff';
+export const MODAL_CLOSE_SEC = 2.5;
